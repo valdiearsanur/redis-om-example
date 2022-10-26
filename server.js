65b389f65ea25a367e47d5630c08ac1e6fd055c2
Valdie Arsanur <muhammad.arsanur@gojek.com>
2022-10-26 21:19:17 +0700

    dockerize project

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ import 'dotenv/config'
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
+import * as dotenv from 'dotenv';
 import { router as collaboratorRouter } from './routers/collaborator-router.js'
 import { router as searchRouter } from './routers/search-router.js'
 
+dotenv.config();
+
 /* create an express app and use JSON */
+const PORT = process.env.PORT || 8080;
+
 const app = new express()
 app.use(express.json())
 
@@ -19,4 +24,4 @@ const swaggerDocument = YAML.load('api.yaml')
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 /* start the server */
-app.listen(8080)
+app.listen(PORT)
